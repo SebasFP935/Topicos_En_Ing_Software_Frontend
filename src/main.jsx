528f6549cd650e2f1d092b7fd1f71325e36fac31
Rodrigo <rodrigoademarsoto@gmail.com>
2026-03-20 01:07:27 -0400

    Integra despliegue en Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App.jsx'
 import './styles/upb-theme.css'
 
@@ -13,6 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       }}
     >
       <App />
+      <Analytics />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 )
